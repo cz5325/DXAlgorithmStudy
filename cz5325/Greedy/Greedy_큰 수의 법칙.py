e9e622ab44ed3133e2bfb3fa76dcc8f20b2a7a0d
cz5325 <cz5325@naver.com>
2024-03-28 09:25:39 +0900

    Greedy_큰 수의 법칙

--- cz5325/Greedy/Greedy_큰 수의 법칙.py
+++ cz5325/Greedy/Greedy_큰 수의 법칙.py
@@ -20,3 +20,23 @@ while True:
     M -= 1
  
 print(result)
+
+
+# 더 효율적인 풀이
+
+N, M, K = map(int,input().split())
+data = list(map(int,input().split()))
+ 
+data.sort()             # 리스트 정렬
+first = data[N - 1]     # 첫 번째 큰 수
+second = data[N - 2]
+ 
+# 가장 큰 수가 더해지는 횟수 계산
+count = int(M / (K + 1)) * K
+count += M % (K + 1)
+ 
+result = 0
+result += (count) * first   # 가장 큰 수더하기
+result += (M - count) * second  # 두 번째로 큰 수 더하기
+ 
+print(result)
