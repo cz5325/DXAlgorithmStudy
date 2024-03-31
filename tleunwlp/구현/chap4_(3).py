0345c7a5e6f0b947038e9c4398912306c55d3b88
sieun0 <evensomere@gmail.com>
2024-03-31 18:56:55 +0900

    chapter4/구현/(3)/게임 개발/수정

--- tleunwlp/구현/chap4_(3).py
+++ tleunwlp/구현/chap4_(3).py
@@ -3,45 +3,54 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 a, b, d = map(int, input().split())
-d_list = [3, 0, 1, 2] # 왼쪽 방향 리스트
-dx = [-1, 0, 1, 0] # 방향 별 x 이동
-dy = [0, 1, 0, -1] # 방향 별 y 이동
+dx = [-1, 0, 1, 0] # 북동남서 방향 별 x 이동
+dy = [0, 1, 0, -1] # 북동남서 방향 별 y 이동
 system = []
 count = 1
 
 for _ in range(n):
     system.append(list(map(int, input().split())))
 
-visited = [[False if value == 0 else True for value in row] for row in system]
+visited = [[0 for _ in range(m)] for _ in range(n)]
+visited[a][b] = 1
 
-row = a
-col = b
-direction = d_list[d] # 왼쪽 방향 회전
+direction = d # 처음 주어진 방향
+
+def turn():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
 # manual 작성
+turn_time = 0
 while True:
-
-    if False not in visited:
-        break
-
-    if not visited[row + dx[direction]][col + dy[direction]]: # 가보지 않은 칸 존재
-        if system[row + dx[direction]][col + dy[direction]] == 0: # 가보지 않은 칸 중 육지
-            row += dx[direction]
-            col += dy[direction]
+    turn()
+    turn_x = a + dx[direction]
+    turn_y = b + dy[direction]
+    # 가보지 않은 칸이 존재한다면
+    if visited[turn_x][turn_y] == 0:
+        # 그 가보지 않은 칸이 육지라면
+        if system[turn_x][turn_y] == 0:
+            visited[turn_x][turn_y] = 1
+            a = turn_x
+            b = turn_y
+            turn_time = 0
             count += 1
-            visited[row][col] = True
+    # 가보지 않은 칸이 없다면
     else:
-        direction = d_list[direction]
-
-    if direction == d or visited[row + dx[direction]][col + dy[direction]]: # 바다인 칸 & 모두 다 가본 칸
-        if system[row][col] != 1:  # 뒤쪽 방향이 바다가 아니라면
-            if direction == 0:
-                row += 1
-            elif direction == 1:
-                col -= 1
-            elif direction == 2:
-                row -= 1
-            elif direction == 3:
-                col += 1
+        turn_time += 1
+    # 만약 네 방향 모두 가본 칸이라면
+    if turn_time == 4:
+        # 그 뒤의 칸이 육지라면
+        turn_x = a - dx[direction]
+        turn_y = b - dy[direction]
+        # 뒤로 갈 수 있다면 이동
+        if system[turn_x][turn_y] == 0:
+            a = turn_x
+            b = turn_y
+            turn_time = 0
+        # 바다라면 이동 못함
         else:
             break
 print(count)
